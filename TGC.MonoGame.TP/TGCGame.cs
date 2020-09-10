f0d3555371630966e98805ebbee05ed7ca39efdd
Ignacio Baptista [Notebook] <ibaptista@est.frba.utn.edu.ar>
2020-09-10 19:18:10 -0300

    Se cargan modelos de ciudad y tanque correctamente

--- TGC.MonoGame.TP/TGCGame.cs
+++ TGC.MonoGame.TP/TGCGame.cs
@@ -27,7 +27,7 @@ namespace TGC.MonoGame.TP
             // Maneja la configuracion y la administracion del dispositivo grafico.
             Graphics = new GraphicsDeviceManager(this);
             // Descomentar para que el juego sea pantalla completa.
-            // Graphics.IsFullScreen = true;
+            //Graphics.IsFullScreen = true;
             // Carpeta raiz donde va a estar toda la Media.
             Content.RootDirectory = "Content";
             // Hace que el mouse sea visible.
@@ -36,9 +36,10 @@ namespace TGC.MonoGame.TP
 
         private GraphicsDeviceManager Graphics { get; }
         private SpriteBatch SpriteBatch { get; set; }
-        private Model Model { get; set; }
+        private Model ModeloTanque { get; set; }
+        private Model ModeloCiudad { get; set; }
         private float Rotation { get; set; }
-        private Matrix World { get; set; }
+        private Matrix WorldTanque { get; set; }
         private Matrix View { get; set; }
         private Matrix Projection { get; set; }
 
@@ -59,8 +60,9 @@ namespace TGC.MonoGame.TP
             // Seria hasta aca.
 
             // Configuramos nuestras matrices de la escena.
-            World = Matrix.CreateRotationY(MathHelper.Pi);
-            View = Matrix.CreateLookAt(Vector3.UnitZ * 150, Vector3.Zero, Vector3.Up);
+            WorldTanque = Matrix.CreateRotationY(MathHelper.Pi) * Matrix.CreateTranslation(10,0,10);
+            View = Matrix.CreateLookAt(new Vector3(30,20,150), new Vector3(30,0,0) , Vector3.Up) ;
+
             Projection =
                 Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, GraphicsDevice.Viewport.AspectRatio, 1, 250);
 
@@ -78,10 +80,14 @@ namespace TGC.MonoGame.TP
             SpriteBatch = new SpriteBatch(GraphicsDevice);
 
             // Cargo el modelo del logo.
-            Model = Content.Load<Model>(ContentFolder3D + "tgc-logo/tgc-logo");
+           // Model = Content.Load<Model>(ContentFolder3D + "tgc-logo/tgc-logo");
+            ModeloTanque = Content.Load<Model>(ContentFolder3D + "tank/tank");
+
+            ModeloCiudad = Content.Load<Model>(ContentFolder3D + "scene/city");
+
             // Obtengo su efecto para cambiarle el color y activar la luz predeterminada que tiene MonoGame.
-            var modelEffect = (BasicEffect) Model.Meshes[0].Effects[0];
-            modelEffect.DiffuseColor = Color.DarkBlue.ToVector3();
+            var modelEffect = (BasicEffect) ModeloTanque.Meshes[0].Effects[0];
+            //modelEffect.DiffuseColor = Color.YellowGreen.ToVector3();
             modelEffect.EnableDefaultLighting();
 
             base.LoadContent();
@@ -101,6 +107,10 @@ namespace TGC.MonoGame.TP
                 //Salgo del juego.
                 Exit();
 
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                //Salgo del juego.
+                Exit();
+
             // Basado en el tiempo que paso se va generando una rotacion.
             Rotation += Convert.ToSingle(gameTime.ElapsedGameTime.TotalSeconds);
 
@@ -116,8 +126,13 @@ namespace TGC.MonoGame.TP
             // Aca deberiamos poner toda la logia de renderizado del juego.
             GraphicsDevice.Clear(Color.Black);
 
+            // Rotacion en y
+            //Matrix.CreateRotationY(Rotation)
+
             //Finalmente invocamos al draw del modelo.
-            Model.Draw(World * Matrix.CreateRotationY(Rotation), View, Projection);
+            ModeloTanque.Draw(WorldTanque * Matrix.CreateScale(5) * Matrix.CreateTranslation(0,-10,0), View, Projection);
+
+            ModeloCiudad.Draw(WorldTanque * Matrix.CreateScale(0.1f), View, Projection);
 
             base.Draw(gameTime);
         }
