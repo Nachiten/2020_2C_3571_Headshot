df838d57455e70cf89b09f5b153a53125e5f5b8c
Ignacio Baptista [Notebook] <ibaptista@est.frba.utn.edu.ar>
2020-10-06 16:55:47 -0300

    Archivos de lfs

--- TGC.MonoGame.TP.Utils/ShapeCreatorHelper.cs
+++ TGC.MonoGame.TP.Utils/ShapeCreatorHelper.cs
@@ -1,34 +1,3 @@
-﻿using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework;
-using System.Collections.Generic;
-using System.Runtime.InteropServices.ComTypes;
-
-namespace TGC.MonoGame.TP.Utils
-{
-    public static class ShapeCreatorHelper
-    {
-
-        public static VertexPositionTexture[] CreatePlane(Vector3 point1, Vector3 point2, Vector3 point3, Vector3 point4, Vector2 textureRepetitions) {
-            var vertices = new VertexPositionTexture[6];
-
-            vertices[0].Position = point1;
-            vertices[1].Position = point2;
-            vertices[2].Position = point3;
-            vertices[3].Position = vertices[1].Position;
-            vertices[4].Position = point4;
-            vertices[5].Position = vertices[2].Position;
-
-            var repitions = 1;
-
-            vertices[0].TextureCoordinate = new Vector2(0, 0);
-            vertices[1].TextureCoordinate = new Vector2(0, repitions);
-            vertices[2].TextureCoordinate = new Vector2(repitions, 0);
-
-            vertices[3].TextureCoordinate = vertices[1].TextureCoordinate;
-            vertices[4].TextureCoordinate = new Vector2(repitions, repitions);
-            vertices[5].TextureCoordinate = vertices[2].TextureCoordinate;
-
-            return vertices;
-        }
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1eeb34da3d41d97491543ff16d32d1df9d53559f5eee37776b342fbe3517f540
+size 1252
